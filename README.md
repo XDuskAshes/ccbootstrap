36895a337b749a97417853708ec026fe27f590ef
XDuskAshes <thegaymeradummy@gmail.com>
2024-12-27 22:51:29 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# ccbootstrap
+A simple program for setting up a CraftOS environment with variably simple configuration.
+
+I made this program since - while yes, most of the time it's unnecessary - it could get some use. It's also a fun messing around project.
+
+## Usage
+Before running `bootstrap.lua`, make sure you have a properly-made `ccbootstrap.json` in the same directory. Here's a pretty loaded example:
+
+```json
+{
+    "meta":{ // Required
+        "name":"Example",
+        "description":"Example ccbootstrap.json",
+        "make_startup":true // Required - make a startup file for things like the path operation
+    },
+    "operations":{ // Required
+        "settings":{ // Optional - Set settings
+            "bios.use_cash":true
+            // So on and so forth
+        },
+        "path":{ // Optional - Add onto or set the shell path
+            "action":"addto", // Required - Can be 'addto' or 'rewrite'
+            "string":"/bin" // Required - Do not start with ':', only add in-between.
+        },
+        "fetch"[ // Optional - fetch files from github repositories
+            {
+                "repo":"torvalds/linux", // Required - The repo
+                "file":"Makefle", // Required - The filename
+                "branch":"master" // Required - The branch to pull from
+            }
+            // So on and so forth
+        ]
+    }
+}
+```
+
+While all `operations` *are* optional, putting no operations just has `ccbootstrap` tell you to do that. Additionally, any *required* components inside of any of the `operations` are, of course, required for that operation.
+
+You also don't need *all* operations present - just one or some.
+
+Below are all operations, in-depth, one-by-one:
+
+### `settings`
+The `settings` operation is essentially just a table of settings and their values in `key:value` pairs. If a setting isn't a valid setting, it gets ignored.
+
+### `path`
+The `path` operation might be a little confusing for some. It modifies the shell path, which is the thing ComputerCraft uses to find and execute programs from anywhere in the system. It has two required components: the `action` and the `string`.
+
+The `action` can either be `addto` or `rewrite`. `addto` will add the `string` to the end of the shell path. `rewrite` will overwrite the shell path to be the `string`.
+
+For example, let's work with a shell path of `.:/rom/programs`, and a string of `/bin`. `addto` would result in the shell path being `.:/rom/programs:/bin`. `rewrite` would result in the shell path being `/bin`.
+
+If the `action` or `string` is not present, the entire operation is skipped.
+
+### `fetch`
+The `fetch` operation is a list full of tables, each with three components: `repo`, `file`, and an optional `branch`. The `repo` component is what Github repository to check, and the `file` component is what file to get from the repository. The `branch` component is what branch to pull from. **If the branch is invalid, ccbootstrap will default to `master`.** If that doesn't exist, **that `fetch` will be skipped.** If a file or repository isn't found, ccbootstrap will keep on and carry on after informing the user.
---
+++ bootstrap.lua
@@ -0,0 +1,164 @@
+-- CCBootstrap - By Dusk
+-- <https://github.com/XDuskAshes/ccbootstrap/>
+-- Intended for use of setting up CraftOS environments
+-- MIT License
+
+-- oh http/https how i hate you ever so deeply,
+-- please go eat unseasoned drywall with
+-- a side of very dry turkey on thanksgiving.
+local function getURL(url) -- Check if something is actually a URL. If so, return yes and it's contents.
+    local resp = http.get(url)
+    if not resp then
+        return false
+    else
+        return true, resp
+    end
+end
+
+local function download(url,file) -- Get the contents of a URL and output to a file.
+    local stat, resp = getURL(url)
+    if not stat then
+        printError("ccbootstrap: Cannot download from URL: "..url.." - invalid")
+    else
+        local handle = fs.open(shell.resolve(file),"w")
+        handle.write(resp.readAll())
+        handle.close()
+    end
+end
+
+local function clearTable(tbl)
+    for k in pairs(tbl) do
+        tbl[k] = nil
+    end
+end
+
+-- grab our 'ccbootstrap.json'
+local bootstrapJSON
+do
+    local JsonConfigHandle = fs.open(shell.resolve("ccbootstrap.json"),"r")
+    if not JsonConfigHandle then
+        error("ccbootstrap: No ccbootstrap.json present in current directory",0)
+    end
+    bootstrapJSON = textutils.unserialiseJSON(JsonConfigHandle.readAll())
+    JsonConfigHandle.close()
+end
+
+print("Name: "..bootstrapJSON.meta.name)
+print("Description: "..bootstrapJSON.meta.description)
+print("Make startup file:",bootstrapJSON.meta.make_startup)
+if not bootstrapJSON.operations then
+    error("ccbootstrap: no operations",0)
+end
+
+if bootstrapJSON.operations.settings then
+    print("Settings")
+    for sKey, vKey in pairs(bootstrapJSON.operations.settings) do
+        print(sKey,"will be",vKey)
+        settings.set(sKey,vKey)
+    end
+    print("End of setttings list - saving")
+    settings.save()
+else
+    print("There is no settings operation.")
+end
+
+if bootstrapJSON.operations.path then
+    if not bootstrapJSON.operations.path.string then
+        printError("ccbootstrap: no operations.path.string - please set this.")
+    else
+        if not bootstrapJSON.operations.path.action then
+            printError("ccbootstrap: no operations.path.action - please set this.")
+        else
+            if bootstrapJSON.operations.path.action == "addto" then
+                print("Adding",bootstrapJSON.operations.path.string,"to path")
+                shell.setPath(shell.path()..":"..bootstrapJSON.operations.path.string)
+            elseif bootstrapJSON.operations.path.action == "rewrite" then
+                print("Setting shell path to",bootstrapJSON.operations.path.string)
+                shell.setPath(bootstrapJSON.operations.path.string)
+            else
+                printError("ccbootstrap: invalid path.action: "..bootstrapJSON.operations.path.action)
+            end
+        end
+    end
+else
+    print("There is no path operation.")
+end
+
+local finalPath = shell.path() -- Used later in making startup
+
+if bootstrapJSON.operations.fetch then
+    local validRepos = {}
+    local validFiles = {}
+    local validReposBranches = {}
+    local finalURLs = {}
+    local threads = {} -- a good teacher taught me how to do parallel in a simple way
+    print("Fetch operation found.")
+    print("This part may be slow, HTTP/HTTPS is not the fastest thing in the world, especially varying with network.")
+    for i, fetchingTable in ipairs(bootstrapJSON.operations.fetch) do
+        threads[#threads+1] = function()
+                local status, foo = getURL("https://github.com/"..fetchingTable.repo)
+                if status then
+                    print("Valid URL: "..fetchingTable.repo)
+                    status, foo = getURL("https://github.com/"..fetchingTable.repo.."/tree/"..fetchingTable.branch)
+                    if not status then
+                        if not getURL("https://github.com/"..fetchingTable.repo.."/tree/master/") then
+                            printError("ccbootstrap: Skipping "..fetchingTable.repo.."\nNo branch of name '"..fetchingTable.branch.."' exists, and cannot default to 'master'.")
+                        else
+                            printError("ccbootstrap: No branch of name '"..fetchingTable.branch.."' exists, although 'master' does, so pulling from that.")
+                            table.insert(validRepos,fetchingTable.repo)
+                            table.insert(validFiles,fetchingTable.file)
+                            table.insert(validReposBranches,"master")
+                        end
+                    else
+                        print("Branch of name '"..fetchingTable.branch.."' exists for '"..fetchingTable.repo.."'")
+                        table.insert(validRepos,fetchingTable.repo)
+                        table.insert(validFiles,fetchingTable.file)
+                        table.insert(validReposBranches,fetchingTable.branch)
+                    end
+                else
+                    printError("ccbootstrap: Invalid: "..fetchingTable.repo)
+                end
+            end
+    end
+    parallel.waitForAll(table.unpack(threads))
+    clearTable(threads)
+    if #validRepos < 1 then
+        printError("ccbootstrap: No valid repositories.")
+    else
+        for repoNum,repoName in pairs(validRepos) do
+            table.insert(finalURLs,"https://raw.githubusercontent.com/"..repoName.."/refs/heads/"..validReposBranches[repoNum].."/"..validFiles[repoNum])
+        end
+
+        for k,v in pairs(finalURLs) do
+            threads[#threads+1] = function()
+                download(v,validFiles[k])
+                if not fs.exists(validFiles[k]) then
+                    printError("ccbootstrap: File not downloaded for unknown reason: "..validFiles[k].."\nFrom url: "..v)
+                end
+            end
+        end
+        parallel.waitForAll(table.unpack(threads))
+        clearTable(threads)
+    end
+else
+    printError("ccbootstrap: No fetch operation.")
+end
+
+if type(bootstrapJSON.meta.make_startup) ~= "boolean" then
+    printError("ccbootstrap: meta.make_startup MUST be true or false")
+else
+    if bootstrapJSON.meta.make_startup == true then -- I'm not 100% on how it'd treat just it existing. this is also just because why not
+        if not bootstrapJSON.operations.path then
+            printError("ccbootstrap: No custom path defined, a generated startup file is unecessary.")
+        else
+            print("Making /startup.lua")
+            local sFile = fs.open("/startup.lua","w")
+            sFile.writeLine("-- Generated by ccbootstrap\n-- Date and time: "..os.date("%D %I:%M %p"))
+            sFile.writeLine("-- Set the path\nshell.setPath('"..finalPath.."')")
+            sFile.close()
+            print("Created")
+        end
+    end
+end
+
+print("Bootstrap finished")
